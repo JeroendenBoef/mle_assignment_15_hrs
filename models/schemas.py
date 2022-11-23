040c489afa45b19a876a8dca122173eaabfcde6f
JeroendenBoef <jeroendenboef1998@gmail.com>
2022-11-23 10:10:35 +0100

    Added pydantic schemas

--- models/schemas.py
+++ models/schemas.py
@@ -2,18 +2,11 @@ import numpy as np
 from typing import List
 from pydantic import BaseModel
 
-# class FeatureArray(BaseModel):
-#     features: np.ndarray = None
-
-#     class Config:
-#         arbitrary_types_allowed = True
-
 
 class PreprocessResponse(BaseModel):
     features: list
 
     class Config:
-        # arbitrary_types_allowed = True
         schema_extra = {"example": {"features": ""}}
 
 
@@ -22,5 +15,4 @@ class InferResponse(BaseModel):
     preprocess: bool = False
 
     class Config:
-        # arbitrary_types_allowed = True
         schema_extra = {"example": {"features": ""}}
