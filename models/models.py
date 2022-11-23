f9125e00c7b6c73509aa0c35fc400595cec551cd
JeroendenBoef <jeroendenboef1998@gmail.com>
2022-11-23 22:39:39 +0100

    Added batched inference

--- models/models.py
+++ models/models.py
@@ -5,7 +5,7 @@ from typing import List
 import pandas as pd
 
 from models.utils import clean_df, convert_to_unix_timedelta
-from models.constants import RELEVANT_COLUMNS, ALL_COLS
+from models.constants import RELEVANT_COLUMNS, ALL_FEATURE_COLS
 
 
 class GBDTModelABC(ABC):
@@ -25,7 +25,7 @@ class GBDTModel(GBDTModelABC):
     def __init__(
         self,
         batch_size: int = 512,
-        model_path: str = "../model_checkpoints/lgb_gbdt_model.pkl",
+        model_path: str = "./model_checkpoints/lgb_gbdt_model.pkl",
     ):
         self.batch_size = batch_size
         self.model_path = model_path
@@ -38,19 +38,24 @@ class GBDTModel(GBDTModelABC):
         return self._model
 
     def preprocess(self, features: list):
-        df = pd.DataFrame(np.array(features), columns=ALL_COLS)
+        df = pd.DataFrame(np.array(features), columns=ALL_FEATURE_COLS)
         df = clean_df(df[RELEVANT_COLUMNS].copy())
         df["date_recorded"] = convert_to_unix_timedelta(df)
-        df.drop(df.columns[len(df.columns) - 1], axis=1, inplace=True)
         return df.values
 
-    def infer(self, features: list, preprocess=False) -> List[int]:
-        output = []
-        for batch in GBDTModel.chunk_batch(features, self.batch_size):
-            if preprocess:
-                batch = self.preprocess(batch)
-            logits = self.model.predict(batch)
-            y_pred = np.argmax(logits, axis=1)
-            output.extend(y_pred)
+    def infer(self, features: list, preprocess=False, batching=True) -> List[int]:
+        if preprocess:
+            features = self.preprocess(features)
+
+        if batching:
+            output = []
+            for batch in GBDTModel.chunk_batch(features, self.batch_size):
+                logits = self.model.predict(batch)
+                y_pred = np.argmax(logits, axis=1)
+                output.extend(y_pred)
+        else:
+            features = [features, features]
+            logits = self.model.predict(features)
+            output = np.argmax(logits, axis=1)[0]
 
         return output
