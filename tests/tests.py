61f3d24a6d14098d155b36f5b69012ca3f0b0148
JeroendenBoef <jeroendenboef1998@gmail.com>
2022-11-24 20:43:43 +0100

    Added sleep timer

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,14 @@
 import unittest
 import requests
+import time
 
 from tests.test_data import UNBATCHED_INPUT_DATA, UNBATCHED_INPUT_LABEL, BATCHED_INPUT_DATA, BATCHED_LABELS
 
 
 class TestClient(unittest.TestCase):
     def setUp(self) -> None:
-        self.url = "http://172.17.0.1:8050/"
+        time.sleep(3)
+        self.url = "http://server:8050/"
 
     def post_request(self, features, preprocess, endpoint):
         data = {
@@ -24,13 +26,13 @@ class TestClient(unittest.TestCase):
         prediction = response.json().get("prediction")
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(prediction), len(UNBATCHED_INPUT_LABEL))
+        self.assertEqual(type(prediction), int)
         self.assertEqual(prediction, UNBATCHED_INPUT_LABEL)
 
     def test_batch_infer(self):
         response = self.post_request(BATCHED_INPUT_DATA, preprocess=False, endpoint="batch_infer")
-        prediction = response.json().get("predictions")
+        predictions = response.json().get("predictions")
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(prediction), len(BATCHED_LABELS))
-        self.assertEqual(prediction, BATCHED_LABELS)
+        self.assertEqual(len(predictions), len(BATCHED_LABELS))
+        self.assertEqual(predictions, BATCHED_LABELS)
